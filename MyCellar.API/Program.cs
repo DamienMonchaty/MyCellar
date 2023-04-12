405be26885fcbf42c043b1bfe311579c1a52e71c
DamienMonchaty <damienmonchaty@hotmail.fr>
2023-04-12 11:49:34 +0200

    modif program.cs

--- MyCellar.API/Program.cs
+++ MyCellar.API/Program.cs
@@ -64,9 +64,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
